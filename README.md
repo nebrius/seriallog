32a7e20097c93f909c4ea29618fdac42641c0b01
Bryan Hughes <bryan@theoreticalideations.com>
2017-04-04 11:34:41 -0700

    First implementation

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# serial-log
-Logs the output from a serial port in real time
+# seriallog
+
+A small command-line utility for logging the output of a serial port in real time.
+
+## Installation
+
+Install globally with npm:
+
+```
+npm install -g seriallog
+```
+
+## Usage
+
+Serial log will often work without any options:
+
+```
+seriallog
+```
+
+However, you can also specify baud rate and port:
+
+```
+seriallog --baudrate 9600 --port /dev/cu.usbserial-12345678
+```
+
+If you do not specify a serial port, it will try and figure it out for you. If there is more than one serial port available, it will prompt you for the port you prefer.
+
+If you do not specify a baudrate, it defaults to 115200.
+
+For more information, run `seriallog --help`
+
+## License
+
+MIT License
+
+Copyright (c) 2017 Bryan Hughes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,131 @@
+/*
+MIT License
+
+Copyright (c) 2017 Bryan Hughes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+const SerialPort = require('serialport');
+const readline = require('readline');
+const yargs = require('yargs');
+
+const DEFAULT_BAUDRATE = 112500;
+
+const VALID_PORT_REGEX = /usb|acm|^com/i;
+
+// Borrowed from Johnny-Five at https://github.com/rwaldron/johnny-five/blob/v0.10.6/lib/board.js#L55
+function filterSerialPorts(ports) {
+  return ports.filter((port) => {
+    // Match only ports that Arduino cares about
+    // ttyUSB#, cu.usbmodem#, COM#
+    return VALID_PORT_REGEX.test(port.comName);
+  });
+}
+
+function watchSerialPort(port, options) {
+  console.log(`\nWatching serial port ${port}`);
+  for (const option in options) {
+    if (!options.hasOwnProperty(option)) {
+      continue;
+    }
+    console.log(`  ${option}: ${options[option]}`);
+  }
+  console.log('---------------------------------------------------------------');
+
+  const portInstance = new SerialPort(port, options);
+
+  // Open errors will be emitted as an error event
+  portInstance.on('error', (err) => {
+    console.error(`Serial port error: ${err.message}`);
+    process.exit(-1);
+  });
+
+  portInstance.on('data', (data) => {
+    process.stdout.write(data);
+  });
+}
+
+SerialPort.list((err, ports) => {
+  if (err) {
+    throw err;
+  }
+
+  const filteredPorts = filterSerialPorts(ports);
+
+  const argv = require('yargs')
+    .usage('Usage: seriallog [options]')
+    .option('port', {
+      alias: 'p',
+      describe: 'The serial port to open',
+      choices: filteredPorts.map((port) => port.comName)
+    })
+    .option('baudrate', {
+      alias: 'b',
+      describe: 'The baudrate of the port',
+      default: DEFAULT_BAUDRATE,
+      number: true
+    })
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+  const options = {
+    baudRate: argv.baudrate
+  };
+
+  if (argv.port) {
+    watchSerialPort(argv.port, options);
+    return;
+  }
+
+  if (filteredPorts.length === 0) {
+    console.error('Could not find any valid serial ports to log');
+  } else if (filteredPorts.length === 1) {
+    watchSerialPort(filteredPorts[0].comName, options);
+  } else {
+    console.log('More than one serial port was detected:\n');
+    for (let i = 0; i < filteredPorts.length; i++) {
+      console.log(`  [${i + 1}]: ${filteredPorts[i].comName}`);
+    }
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    function prompt() {
+      rl.question('\nWhich serial port would you like to use? [1] ', (answer) => {
+        if (answer === '') {
+          rl.close();
+          watchSerialPort(filteredPorts[0].comName, options);
+        } else {
+          const parsedAnswer = parseInt(answer);
+          if (isNaN(parsedAnswer) || parsedAnswer <= 0 || parsedAnswer > filteredPorts.length) {
+            console.error(`"${answer}" is not a valid serial port entry`);
+            prompt();
+          } else {
+            rl.close();
+            watchSerialPort(filteredPorts[parsedAnswer - 1].comName, options);
+          }
+        }
+      });
+    }
+    prompt();
+  }
+});
